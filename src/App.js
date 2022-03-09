5160000b5430351f1293dd82e0c6c079868e8fae
Antoine Carbonnel <antoine@kannounnel.dev>
2022-03-09 10:49:14 +0100

    remove .env from vcs and add it to .gitignore

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from "react";
-import logo from './assets/img/logo.svg';
+
 import Header from "./components/Header";
+import Footer from "./components/Footer"
 
 function App() {
 
   return (
           <div id="app">
             <Header />
+
+            <Footer />
           </div>
   )
 }
